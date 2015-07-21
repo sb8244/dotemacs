86cd306147b62cae71dd4e8ef8be99641050ec7b
Stephen Bussey <steve.bussey@salesloft.com>
2015-07-20 23:23:45 -0400

    bind simp according to their docs

--- custom/07simp.el
+++ custom/07simp.el
@@ -5,7 +5,15 @@
  '(:has (.git)
    :ignore (tmp coverage log vendor .git public/system public/assets)))
 
-(global-set-key [(control p)] 'simp-project-find-file)
+(global-set-key (kbd "C-c f") 'simp-project-find-file)
+(global-set-key (kbd "C-c d") 'simp-project-root-dired)
+(global-set-key (kbd "C-c s") 'simp-project-rgrep)
+(global-set-key (kbd "C-c S") 'simp-project-rgrep-dwim)
+(global-set-key (kbd "C-c b") 'simp-project-ibuffer-files-only)
+(global-set-key (kbd "C-c B") 'simp-project-ibuffer)
+(global-set-key (kbd "C-c C-f") 'simp-project-with-bookmark-find-file)
+(global-set-key (kbd "C-c C-s") 'simp-project-with-bookmark-rgrep)
+(global-set-key (kbd "C-c C-b") 'simp-project-with-bookmark-ibuffer)
 
 (projectile-global-mode)
 (setq projectile-enable-caching t)
